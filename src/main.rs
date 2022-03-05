e7b3518578c79188ff55ed1bca4f666d368471a4
Borja González Seoane <garaje@glezseoane.es>
2022-03-05 20:21:26 +0100

    Implement dotfile reading first draft

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let dotfile = { std::env::var("HOME").unwrap() } + "/.fixpermissions";
+
+    let contents = fs::read_to_string(dotfile).expect(
+        "[ERROR] Dotfile unreachable: create a '.fixpermissions' dotfile in your home \
+        directory and add to it the paths of your documents folders to fix.",
+    );
+
+    println!("{}", contents);
 }
